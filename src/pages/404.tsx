39b3085a440ad9cd3ec805197a309aaa21f9bc98
Joshua Hoeflich <josh@joshuahoeflich.com>
2021-02-25 21:46:00 -0600

    Remove bad dep

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,31 +1,28 @@
 import React from "react";
 import Seo from "../components/SEO";
 import { Flex, Text, Link } from "@chakra-ui/react";
-import { Fade } from "react-awesome-reveal";
 // @ts-ignore
 import John from "../images/john.gif";
 import Header from "../components/Header";
 
 const NotFound: React.FC = () => {
   return (
-    <Fade direction="left">
-      <Flex
-        width="100%"
-        height="100%"
-        flexDirection="column"
-        py="128px"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <img src={John} alt="John signing his name." />
-        <Text py="8px" fontSize="36px">
-          not found
-        </Text>
-        <Link fontSize="28px" href="/">
-          go home
-        </Link>
-      </Flex>
-    </Fade>
+    <Flex
+      width="100%"
+      height="100%"
+      flexDirection="column"
+      py="128px"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <img src={John} alt="John signing his name." />
+      <Text py="8px" fontSize="36px">
+        not found
+      </Text>
+      <Link fontSize="28px" href="/">
+        go home
+      </Link>
+    </Flex>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Seo from "../components/SEO";
 import { Flex } from "@chakra-ui/react";
-import { Fade } from "react-awesome-reveal";
 import LofiJohn from "../components/LofiJohn";
 import Anglerfish from "../components/Anglerfish";
 import Circles from "../components/Circles";
@@ -14,14 +13,12 @@ const HomeBody: React.FC = () => {
   return (
     <Flex width="100%" height="100%" flexDirection="column" py="64px">
       <Flex flexWrap="wrap" justifyContent="center">
-        <Fade direction="left">
-          <LofiJohn />
-          <Anglerfish />
-          <FrenchTheLlama />
-          <PizzaJohn />
-          <Circles />
-          <Infinity />
-        </Fade>
+        <LofiJohn />
+        <Anglerfish />
+        <FrenchTheLlama />
+        <PizzaJohn />
+        <Circles />
+        <Infinity />
       </Flex>
     </Flex>
   );
